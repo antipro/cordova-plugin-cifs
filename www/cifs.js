eae3f51873df759a6d72e0fcde3e7c3f68bbbb12
antipro <antipro@163.com>
2018-03-20 09:05:14 +0800

    add

---
+++ www/cifs.js
@@ -0,0 +1,13 @@
+var exec = require('cordova/exec');
+
+exports.exist = function (arg0, success, error) {
+    exec(success, error, 'Cifs', 'exist', [arg0]);
+};
+
+exports.dir = function (arg0, success, error) {
+    exec(success, error, 'Cifs', 'dir', [arg0]);
+};
+
+exports.download = function (arg0, success, error) {
+    exec(success, error, 'Cifs', 'download', [arg0]);
+};
